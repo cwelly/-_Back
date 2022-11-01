e9724fadbb0f233aa3e929af35bcfd079acddea1
최현호 <potato3884@naver.com>
2022-11-02 00:09:00 +0900

    sql 스키마 변경
    Former-commit-id: 54f8b0f2e76ccd34b407fdc057d4bd72c1203f03

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e096fb57ad27ff92ae7cdcccb60d9ccb7be71b50681b3e3fd89f1268640d832d
+oid sha256:6d5c7d56db41bebf1c972a98ce5d448ce045630101e4d408853b80abd34b891b
 size 313418829
