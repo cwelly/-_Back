00af98b867e485064448e4b83fa8fd740cb08723
ityesmean <KSFJyX8D_rrxeQoPs7YM>
2022-11-04 17:11:46 +0900

    RestAPI 구현
    Former-commit-id: f970c7f98ec49bc6d1b6d4988ea90afe699048f7

--- src/main/resources/static/assets/js/user.js
+++ src/main/resources/static/assets/js/user.js
@@ -6,7 +6,22 @@ function getContextPath() {
 }
 var ctx = getContextPath();
 const login = () => {
-    document.getElementById('login-form').submit();
+    let formData = document.getElementById('login-form');
+    let url = ctx + `/user.do/login/${encodeURIComponent(formData["emailid"].value)}/${encodeURIComponent(formData["emaildomain"].value)}/${encodeURIComponent(formData["password"].value)}`;
+    fetch(url,{method:"POST"})
+        .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.resMsg == "로그인 성공!!!") {
+                    alert("로그인 성공!!!");
+                    location.href = ctx;
+                }
+                else if (data.resMsg =="로그인 실패") {
+                    alert("로그인 실패");
+                    location.href = ctx;
+                }
+            });
+    // document.getElementById('login-form').submit();
 }
 
 const hideToggle = () => {
@@ -30,9 +45,21 @@ const join = () => {
         return;
     }
 
+    let formData = document.getElementById('join-form');
     console.log(user);
-
-    document.getElementById('join-form').submit();
+    let url = ctx + `/user.do/join/${encodeURIComponent(formData["emailid"].value)}/${encodeURIComponent(formData["emaildomain"].value)}/${encodeURIComponent(formData["password"].value)}/${encodeURIComponent(formData["name"].value)}/${encodeURIComponent(formData["addr"].value)}/${encodeURIComponent(formData["phone"].value)}`;
+    fetch(url,{method:"POST"})
+        .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.resMsg == "회원등록완료") {
+                    location.href = ctx;
+                }
+                else if (data.resMsg =="중복회원") {
+                    alert("이미 있는 회원입니다!");
+                }
+            });
+    // document.getElementById('join-form').submit();
 }
 
 const validateInput = ({emailId, password, username, address, phone}) => {
@@ -54,8 +81,21 @@ const update = () => {
         alert("빈칸이 없도록 입력해주세요!");
         return;
     }
-
-	document.getElementById('modify-form').submit();
+    let formData = document.getElementById('modify-form');
+    // fetch(ctx + `/user.do/update/${formData.get('emailid')}/${formData.get('emaildomain')}/${formData.get('password')}/${formData.get('name')}/${formData.get('addr')}/${formData.get('phone')}`)
+    // fetch(ctx + `/user.do/update/${formData.emailid.value}/${formData.emaildomain.value}/${formData.password.value}/${formData.name.value}/${formData.addr.value}/${formData.phone.value}`)
+    let url = ctx + `/user.do/update/${encodeURIComponent(formData["emailid"].value)}/${encodeURIComponent(formData["emaildomain"].value)}/${encodeURIComponent(formData["password"].value)}/${encodeURIComponent(formData["name"].value)}/${encodeURIComponent(formData["addr"].value)}/${encodeURIComponent(formData["phone"].value)}`;
+    console.log(formData);
+    console.log(url);
+    fetch(url,{method:"POST"})
+        .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.resMsg == "개인정보수정완료") {
+                    location.href = ctx;
+                }
+            });
+	// document.getElementById('modify-form').submit();
 }
 
 const modalHide = (id) => {
@@ -79,12 +119,20 @@ const getValueFromInput = (query) => {
     return document.querySelector(query).value;
 }
 
-document.querySelector("#modal-login-btn").addEventListener("click", login, true);
+document.querySelector("#modal-login-btn").addEventListener("click", login);
 document.querySelector("#modal-join-btn").addEventListener("click", join);
 
 document.querySelector("#logout-btn").addEventListener("click", () => {
     if(confirm("로그아웃 하시겠습니까?")) {
-        location.href = ctx+"/user.do/logout";
+        // location.href = ctx + "/user.do/logout";
+        fetch(ctx + `/user.do/logout`)
+        .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.resMsg == "로그아웃완료") {
+                    location.href = ctx;
+                }
+            });
     }
 })
 
@@ -93,7 +141,16 @@ const withdrawal = () => {
 //        localStorage.removeItem(loginedUser.email);
 //        alert("탈퇴 성공!!");
 //        hideToggle();
-    	location.href= ctx+"/user.do/withdrawal";
+        // location.href = ctx + "/user.do/withdrawal";
+        
+        fetch(ctx + "/user.do/withdrawal")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.resMsg == "삭제완료") {
+                    location.href = ctx;
+                }
+            });
     }
 }
 
