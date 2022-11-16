9a8155354b61f431e8c0f5c906ab5953905080b9
ityesmean <KSFJyX8D_rrxeQoPs7YM>
2022-11-16 09:38:57 +0900

    Cross origin 에러 구문 추가
    Former-commit-id: f980a48480087b5126d6a9b4949851b56e47fa1a

--- src/main/java/com/ssafy/passproject/controller/UserController.java
+++ src/main/java/com/ssafy/passproject/controller/UserController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,7 @@ import com.ssafy.passproject.dto.User;
 import com.ssafy.passproject.dto.UserInfo;
 import com.ssafy.passproject.service.RegionService;
 import com.ssafy.passproject.service.UserService;
-
+@CrossOrigin(origins = {"*"},maxAge = 6000)
 @Controller
 @RequestMapping("/user.do")
 public class UserController {
