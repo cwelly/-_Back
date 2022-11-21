d9a0b3c10b483ecf97b6dbb28647567c851ada78
potato3884 <potato3884@naver.com>
2022-11-21 09:23:22 +0900

    스키마 업데이트

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8a3e4bd633726dc91d2c18ceafbfbb8b60bd0d7810c3ee35d4ca59a880bd2d0
-size 313418874
+oid sha256:cc8a723fe4e5a542d30b4f6fc7c6ce9a61b9cd90dd88850c780e1d1bf51a4566
+size 313419067
